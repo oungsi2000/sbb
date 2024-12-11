33b99b36c22267c54e887fc894219e552d66907f
jhs512 <jangka512@gmail.com>
2024-12-11 14:02:58 +0900

    step 1

--- src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,6 +15,7 @@ import static jakarta.persistence.GenerationType.IDENTITY;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
 public class Post {
     @Id
     @GeneratedValue(strategy = IDENTITY) // AUTO_INCREMENT
