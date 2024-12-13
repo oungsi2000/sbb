043c1ddfd9f893b2d1856d6f699147090db6221d
jhs512 <jangka512@gmail.com>
2024-12-13 14:52:16 +0900

    orphan 1

--- src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
+++ src/main/java/com/ll/jpa/domain/post/post/entity/Post.java
@@ -44,7 +44,7 @@ public class Post {
     private boolean blind;
 
     @Builder.Default
-    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    @OneToMany(mappedBy = "post", cascade = {CascadeType.PERSIST, CascadeType.REMOVE}, orphanRemoval = true)
     private List<PostComment> comments = new ArrayList<>();
 
     public void addComment(String content) {
@@ -55,5 +55,9 @@ public class Post {
                 .build();
 
         comments.add(postComment);
+    }
+
+    public boolean removeComment(PostComment comment) {
+        return comments.remove(comment);
     }
 }
