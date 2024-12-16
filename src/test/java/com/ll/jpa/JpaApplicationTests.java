410f6b0198f0a7202662c100a41a6f83471f8c42
jhs512 <jangka512@gmail.com>
2024-12-16 10:24:38 +0900

    dev, test

--- src/test/java/com/ll/jpa/JpaApplicationTests.java
+++ src/test/java/com/ll/jpa/JpaApplicationTests.java
@@ -2,12 +2,14 @@ package com.ll.jpa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class JpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
