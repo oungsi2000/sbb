b28afe73741732e13db2f74e4c6a2a7033c23d70
jhs512 <jangka512@gmail.com>
2024-12-16 13:12:11 +0900

    work

--- src/main/java/com/ll/jpa/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/ll/jpa/domain/post/post/repository/PostRepository.java
@@ -1,6 +1,8 @@
 package com.ll.jpa.domain.post.post.repository;
 
 import com.ll.jpa.domain.post.post.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -19,4 +21,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findTop2ByTitleLikeOrderByIdDesc(String title);
 
     List<Post> findTop2ByOrderByIdDesc();
+
+    Page<Post> findByTitleLike(String title, Pageable pageable);
 }
